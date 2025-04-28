7182117526a98c3d695988b91cbb06ca96cdb23c
Manoj <manojpolevault1202@gmail.com>
2025-04-28 16:32:15 +0530

    icon added

--- src/Layouts/Home.js
+++ src/Layouts/Home.js
@@ -6,6 +6,8 @@ import OurService from "./OurService";
 import Success from "./Success";
 import Testimonial from "./Testimonial";
 import shopNow from "../assets/shopNow.png";
+import phonecall from "../assets/phone-call.png";
+import whatsapp from "../assets/whatsapp.png";
 
 import { Link } from "react-router-dom";
 
@@ -25,7 +27,28 @@ const Home = () => {
         </Link>
       </div>
 
-     
+      {/* WhatsApp and Call Buttons */}
+      <div className="fixed top-1/2 left-3 transform -translate-y-1/2 z-50 flex flex-col gap-4">
+        <a href="tel:+9445280054" className="rounded-full animate-pulse">
+          <img
+            src={phonecall}
+            alt="Call Now"
+            className="w-8 h-8 sm:w-12 sm:h-12 object-contain"
+          />
+        </a>
+        <a
+          href="https://api.whatsapp.com/send?phone=9444813377"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="rounded-full animate-pulse"
+        >
+          <img
+            src={whatsapp}
+            alt="WhatsApp"
+            className="w-8 h-8 sm:w-12 sm:h-12 object-contain"
+          />
+        </a>
+      </div>
 
       <Main />
       <MoveImage />
